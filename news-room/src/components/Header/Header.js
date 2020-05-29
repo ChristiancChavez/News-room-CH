15c2492dcf9d2ae684271b0f30fe4ab95b045bb4
Christian Chavez <christiancchavez10@hotmail.com>
2020-05-28 20:58:18 -0500

    add sass

---
+++ news-room/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Search from '../Search/Search';
+// import './Header.scss';
+
+const Header = props => {
+    return (
+        <div className="">
+            <h1>New Feeds</h1>
+            {/* <Search /> */}
+        </div>
+    );
+};
+
+export default Header;
